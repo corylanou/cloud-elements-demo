d53167ef6f887864a90cecf0a03e7c7155458911
Cory LaNou <cory@lanou.com>
2015-03-07 10:29:22 -0700

    02 column format output

--- 02_consuming/main.go
+++ 02_consuming/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strings"
+	"text/tabwriter"
 
 	"github.com/corylanou/cloud-elements"
 	"github.com/davecgh/go-spew/spew"
@@ -15,20 +18,43 @@ func main() {
 	}
 	credentials.Elements[cloudElements.GOOGLE_DRIVE] = os.Getenv("CE_GOOGLEDRIVE")
 	credentials.Elements[cloudElements.DROPBOX] = os.Getenv("CE_DROPBOX")
-	spew.Dump(credentials)
 
 	client := cloudElements.NewClient(credentials)
 
 	googleFiles, err := client.CloudFiles.Contents("/", cloudElements.GOOGLE_DRIVE)
 	if err != nil {
 		spew.Dump(err)
+		return
 	}
-	spew.Dump(googleFiles)
+	writeCSV(googleFiles, "google")
 
 	dropboxFiles, err := client.CloudFiles.Contents("/", cloudElements.DROPBOX)
 	if err != nil {
 		spew.Dump(err)
+		return
 	}
-	spew.Dump(dropboxFiles)
+	writeCSV(dropboxFiles, "dropbox")
+
+}
+
+// Write the contents out nicely in columns
+func writeCSV(files []cloudElements.CloudFile, source string) {
+	fmt.Println()
+	columnNames := []string{"source", "id", "name", "directory"}
+	rows := []string{}
+
+	rows = append(rows, strings.Join(columnNames, "\t"))
+
+	for _, f := range files {
+		row := []string{source, f.Id, f.Name, fmt.Sprintf("%v", f.Directory)}
+		rows = append(rows, strings.Join(row, "\t"))
+	}
+
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 1, '\t', 0)
+	for _, r := range rows {
+		fmt.Fprintln(w, r)
+	}
+	w.Flush()
 
 }
